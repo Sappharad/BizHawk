0b53cb6c2dd492ca7747871c1392bba63c618b85
Sappharad <sappharad@Titanium.local>
2017-11-14 22:21:31 -0600

    Preliminary EtoHawk project against Xamarin.Mac and BizHawk 2 master. Requires Lua to be platformed away.

---
+++ BizHawk.Client.EtoHawk/Config/FirmwaresConfig.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Eto.Forms;
+using BizHawk.Common;
+using BizHawk.Client.Common;
+using BizHawk.Emulation.Common;
+using System.Collections.Generic;
+
+namespace BizHawk.Client.EtoHawk
+{
+	internal class FirmwareRow
+	{
+		public FirmwareDatabase.FirmwareRecord Record { get; set; }
+		public string SystemId { get { return Record.SystemId; } }
+		public string FirmwareId { get { return Record.FirmwareId; } }
+		public string Description { get { return Record.Descr; } }
+		public string ResolvedWith { get; set; }
+		public string Location { get; set; }
+		public long Size { get; set; }
+		public string Hash { get; set; }
+	}
+
+	public partial class FirmwaresConfig  : Dialog<bool>
+	{
+		public FirmwaresConfig ()
+		{
+			InitializeComponent();
+			FirmwaresConfig_Load ();
+		}
+
+		private void FirmwaresConfig_Load()
+		{
+			//we'll use this font for displaying the hash, so they dont look all jagged in a long list
+			/*fixedFont = new Font(new FontFamily("Courier New"), 8);
+			boldFont = new Font(lvFirmwares.Font, FontStyle.Bold);
+			boldFixedFont = new Font(fixedFont, FontStyle.Bold);*/
+
+			//populate listview from firmware DB
+			//var groups = new Dictionary<string, ListViewGroup>();
+			List<FirmwareRow> firmwareList = new List<FirmwareRow>();
+			foreach (FirmwareDatabase.FirmwareRecord fr in FirmwareDatabase.FirmwareRecords)
+			{
+				var lvi = new FirmwareRow();
+				lvi.Record = fr;
+				firmwareList.Add(lvi);
+
+				//build the groups in the listview as we go:
+				/*if (!groups.ContainsKey(fr.systemId))
+				{
+					string name;
+					if (!SystemGroupNames.TryGetValue(fr.systemId, out name))
+						name = "FIX ME (FirmwaresConfig.cs)";
+					lvFirmwares.Groups.Add(fr.systemId, name);
+					var lvg = lvFirmwares.Groups[lvFirmwares.Groups.Count - 1];
+					groups[fr.systemId] = lvg;
+				}
+				lvi.Group = groups[fr.systemId];*/
+			}
+			gvFirmwares.DataStore = firmwareList;
+
+			//now that we have some items in the listview, we can size some columns to sensible widths
+			/*lvFirmwares.AutoResizeColumn(1, ColumnHeaderAutoResizeStyle.ColumnContent);
+			lvFirmwares.AutoResizeColumn(2, ColumnHeaderAutoResizeStyle.ColumnContent);
+			lvFirmwares.AutoResizeColumn(3, ColumnHeaderAutoResizeStyle.ColumnContent);
+
+			if (TargetSystem != null)
+			{
+				WarpToSystemId(TargetSystem);
+			}
+
+			RefreshBasePath();*/
+		}
+
+		private void SetCustom_Click (object sender, EventArgs e)
+		{
+			//Unfortunately, Eto seems to have no way to tell where you right-clicked on a grid via the context menu.
+			//I will probably get rid of the context menu and just add set/clear buttons to a toolbar, but not right now.
+			if (gvFirmwares.SelectedItem != null && gvFirmwares.SelectedItem is FirmwareRow)
+			{
+				OpenFileDialog ofd = new OpenFileDialog();
+				if (ofd.ShowDialog(this) == DialogResult.Ok) 
+				{
+					FirmwareRow fr = (FirmwareRow)gvFirmwares.SelectedItem;
+					Global.Config.FirmwareUserSpecifications[fr.Record.ConfigKey] = ofd.FileName;
+					DoScan();
+				}
+			}
+		}
+
+		private void ClearCustom_Click (object sender, EventArgs e)
+		{
+			if (gvFirmwares.SelectedItem != null && gvFirmwares.SelectedItem is FirmwareRow) 
+			{
+				FirmwareRow fr = (FirmwareRow)gvFirmwares.SelectedItem;
+				Global.Config.FirmwareUserSpecifications.Remove(fr.Record.ConfigKey);
+				DoScan();
+			}
+		}
+
+		FirmwareManager Manager { get { return Global.FirmwareManager; } }
+		private void DoScan()
+		{
+			//lvFirmwares.BeginUpdate();
+			Manager.DoScanAndResolve();
+
+			//for each type of firmware, try resolving and record the result
+			foreach (FirmwareRow lvi in gvFirmwares.DataStore)
+			{
+				var fr = lvi.Record;
+				var ri = Manager.Resolve(fr, true);
+
+				if (ri == null)
+				{
+					//lvi.Description = "Missing!";
+				}
+				else
+				{
+					//lazy substring extraction. really should do a better job
+					var basePath = PathManager.MakeAbsolutePath(Global.Config.PathEntries.FirmwaresPathFragment, null) + System.IO.Path.DirectorySeparatorChar;
+
+					var path = ri.FilePath.Replace(basePath, "");
+
+					//bolden the item if the user has specified a path for it
+					bool bolden = ri.UserSpecified;
+
+					//set columns based on whether it was a known file
+					if (ri.KnownFirmwareFile == null)
+					{
+						//lvi.ImageIndex = idUnsure;
+						//lvi.Description = "-custom-";
+					}
+					else
+					{
+						//lvi.ImageIndex = idOk;
+						//lvi.Description = ri.KnownFirmwareFile.descr;
+					}
+
+					//if the user specified a file but its missing, mark it as such
+					/*if (ri.Missing)
+					{
+						lvi.ImageIndex = idMissing;
+						lvi.ToolTipText = "Missing!";
+					}*/
+
+					//if the user specified a known firmware file but its for some other firmware, it was probably a mistake. mark it as suspicious
+					/*if (ri.KnownMismatching)
+						lvi.ImageIndex = idUnsure;*/
+
+					lvi.Location = path;
+
+					lvi.Size = ri.Size;
+
+					if (ri.Hash != null) lvi.Hash = "sha1:" + ri.Hash;
+					else lvi.Hash = "";
+				}
+			}
+
+			//lvFirmwares.EndUpdate();
+		}
+	}
+}
+
---
+++ BizHawk.Client.EtoHawk/Config/InputWidget.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Eto;
+using Eto.Forms;
+using Eto.Drawing;
+using BizHawk.Client.EtoHawk;
+using System.Linq;
+using System.Threading;
+
+namespace EtoHawk.Config
+{
+    public sealed class InputWidget : TextBox
+    {
+        // TODO: when binding, make sure that the new key combo is not in one of the other bindings
+        private readonly List<string> _bindings = new List<string>();
+
+        public InputCompositeWidget CompositeWidget;
+        public ControllerConfig ParentConfig { get; set; }
+
+        public class SpecialBindingInfo
+        {
+            public string BindingName;
+            public string TooltipText;
+        }
+
+        /// <summary>
+        /// These bindings get ignored by the widget and can only be entered by SetBinding() via the contextmenu from the InputCompositeWidget
+        /// </summary>
+        public static readonly SpecialBindingInfo[] SpecialBindings = {
+			 new SpecialBindingInfo { BindingName = "Escape", TooltipText = "Binds the Escape key" },
+			new SpecialBindingInfo { BindingName = "WMouse L", TooltipText =  "Binds the left mouse button"},
+				new SpecialBindingInfo { BindingName = "WMouse M", TooltipText =  "Binds the middle mouse button"},
+				new SpecialBindingInfo { BindingName = "WMouse R", TooltipText =  "Binds the right mouse button"},
+			new SpecialBindingInfo { BindingName = "WMouse 1", TooltipText =  "Binds the mouse auxiliary button 1" }, 
+				new SpecialBindingInfo {	BindingName = "WMouse 2", TooltipText =  "Binds the mouse auxiliary button 2" },
+		};
+
+
+        public InputWidget(ControllerConfig parent)
+        {
+            ParentConfig = parent;
+            //ContextMenu = new ContextMenu();
+            ClearBindings();
+            AutoTab = true;
+            Cursor = Cursors.Arrow;
+        }
+
+        public bool AutoTab { get; set; }
+        public string WidgetName { get; set; }
+
+        public string Bindings
+        {
+            get
+            {
+                return Text;
+            }
+            set
+            {
+                ClearBindings();
+                var newBindings = value.Trim().Split(',');
+                _bindings.AddRange(newBindings);
+                UpdateLabel();
+            }
+        }
+
+        public void ClearAll()
+        {
+            ClearBindings();
+            base.Text = "";
+        }
+
+        private void ClearBindings()
+        {
+            _bindings.Clear();
+        }
+
+        protected override void OnGotFocus(EventArgs e)
+        {
+            base.OnGotFocus(e);
+
+            ParentConfig.FocusedWidget = this;
+
+            //Console.WriteLine (this.Bindings + " got focus");
+            BackgroundColor = Color.FromArgb(unchecked((int)0xFFC0FFFF)); // Color.LightCyan is too light on Windows 8, this is a bit darker
+        }
+
+        protected override void OnLostFocus(EventArgs e)
+        {
+            base.OnLostFocus(e);
+            //Console.WriteLine (this.Bindings + " lost focus");
+            UpdateLabel();
+            BackgroundColor = Colors.White; //SystemColors.Window;
+        }
+
+        public void HandleMappingInput(string bindingStr){
+            //Differs from SetBinding because it has the logic in it.
+            //has special meaning for the binding UI system (clear it).
+            //you can set it through the special bindings dropdown menu
+            if (bindingStr == "Escape")
+            {
+                EraseMappings();
+                Increment();
+                return;
+            }
+
+            //seriously, we refuse to allow you to bind this to anything else.
+            if (bindingStr == "Alt+F4")
+            {
+                return;
+            }
+
+            //ignore special bindings
+            foreach (var spec in SpecialBindings)
+                if (spec.BindingName == bindingStr)
+                    return;
+
+            if (!IsDuplicate(bindingStr))
+            {
+                if (AutoTab)
+                {
+                    ClearBindings();
+                }
+
+                _bindings.Add(bindingStr);
+            }
+
+            UpdateLabel();
+            Increment();
+        }
+
+        public void EraseMappings()
+        {
+            ClearBindings();
+            Text = string.Empty;
+        }
+
+        /// <summary>
+        /// sets a binding manually. This may not be implemented quite right.
+        /// </summary>
+        public void SetBinding(string bindingStr)
+        {
+            _bindings.Add(bindingStr);
+            UpdateLabel();
+            Increment();
+        }
+
+        private bool IsDuplicate(string binding)
+        {
+            return _bindings.FirstOrDefault(x => x == binding) != null;
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            if (e.Key == Keys.F4 && e.Modifiers == Keys.Alt)
+            {
+                base.OnKeyUp(e);
+            }
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Keys.F4 && e.Modifiers == Keys.Alt)
+            {
+                base.OnKeyDown(e);
+                return;
+            }
+
+            e.Handled = true;
+        }
+
+        // Advances to the next widget depending on the autotab setting
+        public void Increment()
+        {
+            if (AutoTab)
+            {
+                CompositeWidget.TabNext();
+            }
+        }
+
+        public void Decrement()
+        {
+            if (AutoTab)
+            {
+                Parent.SelectNextControl(this, false, true, true, true);
+            }
+        }
+
+        public void UpdateLabel()
+        {
+            Text = string.Join(",", _bindings.Where(str => !string.IsNullOrWhiteSpace(str)));
+            CompositeWidget.RefreshTooltip();
+        }
+        
+        /*protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }*/
+        
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            if (e.Delta.Height > 0)
+            {
+                Decrement();
+            }
+            else
+            {
+                Increment();
+            }
+
+            base.OnMouseWheel(e);
+        }
+
+        /*protected override void OnGotFocus(EventArgs e)
+        {
+            HideCaret(Handle);
+        }*/
+
+        /*protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            return !(keyData.ToString() == "F4" || keyData.ToString().Contains("Alt"));
+        }*/
+    }
+}
